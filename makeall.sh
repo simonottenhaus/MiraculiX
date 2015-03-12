f2394d6061c92f5192ae33069f427e9954e3d4dd
Markus Grotz <markus.grotz@kit.edu>
2015-03-12 13:37:52 +0100

    added simple parameter passing to makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -6,27 +6,19 @@ source $BASEDIR/colors.rc
 
 cd $ARMARX_DIR
 
+p="-j4"
+if [[ $# -gt 0 ]]; then
+    p=$*
+fi
+
 for PKG_DIR in $PKG_NAMES; do
 	echo -e "${BWhi}============================${RCol}"
 	echo -e "${BWhi}Building ${PKG_DIR} ${RCol}"
 	echo -e "${BWhi}============================${RCol}"
+
 	cd $PKG_DIR/build
-	
 	cmake ..
-	if [ $? -ne 0 ]; then
-		echo -e "${BRed}============================${RCol}"
-		echo -e "${BRed}${PKG_DIR} CMAKE FAILED ${RCol}"
-		echo -e "${BRed}============================${RCol}"
-		exit 1
-	fi
-	
-	make -j4
-	if [ $? -ne 0 ]; then
-		echo -e "${BRed}============================${RCol}"
-		echo -e "${BRed}${PKG_DIR} BUILD FAILED ${RCol}"
-		echo -e "${BRed}============================${RCol}"
-		exit 1
-	fi
+	make $p
     cd -
 done
 
