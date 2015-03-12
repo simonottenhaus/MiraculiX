e59e5031aa9b592c83b7b7690f2f24117caf380e
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2015-03-12 17:34:20 +0100

    start & stop script for ice & mongodb on lab pcs

---
+++ halt.sh
@@ -0,0 +1,4 @@
+~/home/armarx/Core/build/bin/ice-stop.sh
+#~/home/armarx/MemoryX/build/bin/mongod.sh repair
+~/home/armarx/MemoryX/build/bin/mongod.sh stop
+
---
+++ run.sh
@@ -0,0 +1,4 @@
+~/home/armarx/Core/build/bin/ice-start.sh
+#~/home/armarx/MemoryX/build/bin/mongod.sh repair
+~/home/armarx/MemoryX/build/bin/mongod.sh start
+
