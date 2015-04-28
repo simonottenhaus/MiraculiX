73fa8b014725161830e6993ed9df79283d47c5a6
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2015-04-28 10:25:46 +0200

    fast make script (without cmake, faster on lab pcs with slow nfs io)

---
+++ fastmakeall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+BASEDIR=`pwd`
+popd > /dev/null
+
+source $BASEDIR/config.rc
+source $BASEDIR/colors.rc
+
+p="-j4"
+if [[ $# -gt 0 ]]; then
+    p=$*
+fi
+
+for PKG_DIR in $PKG_NAMES; do
+	echo -e "${BWhi}============================${RCol}"
+	echo -e "${BWhi}Building ${PKG_DIR} ${RCol}"
+	echo -e "${BWhi}============================${RCol}"
+	cd $ARMARX_DIR/$PKG_DIR/build
+	
+	make $p
+	if [ $? -ne 0 ]; then
+		echo -e "${BRed}============================${RCol}"
+		echo -e "${BRed}${PKG_DIR} BUILD FAILED ${RCol}"
+		echo -e "${BRed}============================${RCol}"
+		exit 1
+	fi
+done
+
+
