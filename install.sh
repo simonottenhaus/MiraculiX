c14c5d5b4979150e2f89382e3fbdec7e65af87e3
Markus Grotz <markus-grotz@web.de>
2015-05-01 11:31:54 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 pushd `dirname $0` > /dev/null
 BASEDIR=`pwd`
@@ -6,6 +6,9 @@ popd > /dev/null
 
 source $BASEDIR/config.rc
 source $BASEDIR/colors.rc
+
+git clone https://armarx:armarx@i61wiki.itec.uka.de/git/armarx.git  /tmp/armarx
+
 
 cd $ARMARX_DIR
 repo init -u /tmp/armarx
