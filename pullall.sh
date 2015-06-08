eb569cc49e07e392288fa2d2dcabb48ffee88c1e
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2015-06-08 14:12:08 +0200

    changed pullall log

--- pullall.sh
+++ pullall.sh
@@ -9,13 +9,16 @@ source $BASEDIR/colors.rc
 
 for PKG_DIR in $PKG_NAMES; do
 	cd $ARMARX_DIR/$PKG_DIR
-    TIME=$(date -u +%FT%TZ)
+    TIME=$(date -u +"%F %T")
     GIT_VERSION=$(git describe --always --dirty --long --tags)
     GIT_HEAD=$(git rev-parse HEAD)
 	echo -e "${BWhi}${PKG_DIR}${RCol} - ${GIT_VERSION}"
-    echo -n "${TIME} - updating ${PKG_DIR} ${GIT_VERSION} from ${GIT_HEAD} " >> $LOG_FILE
 	git pull
     GIT_HEAD_NEW=$(git rev-parse HEAD)
-    echo " -> ${GIT_HEAD_NEW} " >> $LOG_FILE
+    if [ "$GIT_HEAD" != "$GIT_HEAD_NEW" ]; then
+		echo "${TIME} - ${PKG_DIR}: updating from ${GIT_VERSION} ${GIT_HEAD}  -> ${GIT_HEAD_NEW}" >> $LOG_FILE
+	else
+		echo "${TIME} - ${PKG_DIR}: no remote changes" >> $LOG_FILE
+	fi
 done
 
