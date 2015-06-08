21783f48134df232d97de8d70c752cf3372568b4
Markus Grotz <markus.grotz@kit.edu>
2015-06-08 12:12:41 +0200

    added simple logging feature to pullall script

--- pullall.sh
+++ pullall.sh
@@ -8,8 +8,14 @@ source $BASEDIR/config.rc
 source $BASEDIR/colors.rc
 
 for PKG_DIR in $PKG_NAMES; do
-	echo -e "${BWhi}${PKG_DIR}${RCol}"
 	cd $ARMARX_DIR/$PKG_DIR
+    TIME=$(date -u +%FT%TZ)
+    GIT_VERSION=$(git describe --always --dirty --long --tags)
+    GIT_HEAD=$(git rev-parse HEAD)
+	echo -e "${BWhi}${PKG_DIR}${RCol} - ${GIT_VERSION}"
+    echo -n "${TIME} - updating ${PKG_DIR} ${GIT_VERSION} from ${GIT_HEAD} " >> $LOG_FILE
 	git pull
+    GIT_HEAD_NEW=$(git rev-parse HEAD)
+    echo " -> ${GIT_HEAD_NEW} " >> $LOG_FILE
 done
 
