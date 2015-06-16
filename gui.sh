bd93c1f644abe00e5d40a0bd0a42c0552c296d2c
Markus Grotz <markus.grotz@kit.edu>
2015-06-16 18:44:36 +0200

    fixed gui path

--- gui.sh
+++ gui.sh
@@ -1,1 +1,11 @@
-./Gui/build/bin/ArmarXGuiRun --ArmarX.GuiConfigFile=_defaultGui.armarxgui
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+BASEDIR=`pwd`
+popd > /dev/null
+
+source $BASEDIR/config.rc
+source $BASEDIR/colors.rc
+
+
+$ARMARX_DIR/Gui/build/bin/ArmarXGuiRun --ArmarX.GuiConfigFile=_defaultGui.armarxgui
