4bff88f55bc0c9527edb3988c9ae5fd72b4c49aa
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2015-07-16 16:26:09 +0200

    cmake errors in red

--- makeall.sh
+++ makeall.sh
@@ -18,7 +18,7 @@ for PKG_DIR in $PKG_NAMES; do
 	echo -e "${BWhi}============================${RCol}"
 	cd $ARMARX_DIR/$PKG_DIR/build
 	
-	cmake ..
+	cmake .. 2> >(while read line; do echo -e "\e[01;31m$line\e[0m" >&2; done)
 	if [ $? -ne 0 ]; then
 		echo -e "${BRed}============================${RCol}"
 		echo -e "${BRed}${PKG_DIR} CMAKE FAILED ${RCol}"
