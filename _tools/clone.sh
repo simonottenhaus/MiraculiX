8265f61329493644232f47116424c3cb000b29da
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2015-09-22 15:33:06 +0200

    clone script for initial checkout

---
+++ _tools/clone.sh
@@ -0,0 +1,16 @@
+git clone https://gitlab.com/ArmarX/ArmarXDoc.git
+git clone https://gitlab.com/ArmarX/ArmarXCore.git
+git clone https://gitlab.com/ArmarX/ArmarXGui.git
+git clone https://gitlab.com/ArmarX/RobotAPI.git
+git clone https://gitlab.com/ArmarX/MemoryX.git
+git clone https://gitlab.com/ArmarX/ArmarXDB.git
+git clone https://gitlab.com/ArmarX/VisionX.git
+git clone https://gitlab.com/ArmarX/RobotComponents.git
+git clone https://gitlab.com/ArmarX/ArmarXSimulation.git
+git clone https://gitlab.com/ArmarX/RobotSkillTemplates.git
+
+git clone https://i61wiki.itec.uka.de/git/Spoac
+git clone https://i61wiki.itec.uka.de/git/Speechx
+git clone https://i61wiki.itec.uka.de/git/Armar3
+git clone https://i61wiki.itec.uka.de/git/Armar4
+
