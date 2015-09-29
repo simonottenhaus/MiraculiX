65736391ab4775ef09d0061ada2b486f87b6ff50
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2015-09-29 16:55:29 +0200

    pb alias to go to build dir

---
+++ _aliases/projectbuild.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+IFS=" " read -a projects <<< "$AX_PACKAGES"
+
+if [ -z $1 ]
+then
+	cd "$ArmarX_DIR"
+else
+	FOUND=0
+	for i in "${projects[@]}"
+	do
+		PROJECT_WITH_PREFIX=armarx$1
+		if [[ ${i,,} = $1* ]] || [[ ${i,,} = $PROJECT_WITH_PREFIX* ]]
+		then
+			cd $ArmarX_DIR/$i/build
+			FOUND=1
+			break
+		fi
+	done
+	if [ $FOUND != 1 ]
+	then
+		for i in "${projects[@]}"
+		do
+			if [[ ${i,,} = *$1* ]]
+			then
+				cd $ArmarX_DIR/$i/build
+				break
+			fi
+		done
+	fi
+fi
+
+unset IFS
+unset projects
+unset PROJECT_WITH_PREFIX
+unset FOUND
