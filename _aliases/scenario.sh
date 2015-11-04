8738f8fb805f91910d3c3fe8d460fe427a2481a8
Valerij Wittenbeck <valerij.wittenbeck@student.kit.edu>
2015-11-04 19:11:31 +0100

    "scenario" script now searches test/tests subfolders as well, it also no supports infix search

--- _aliases/scenario.sh
+++ _aliases/scenario.sh
@@ -2,32 +2,45 @@
 
 IFS=" " read -a projects <<< "$AX_PACKAGES"
 
-AX_SCENARIO_FOUND=false
+AX_SCENARIO_FOUND=0
 
 for i in "${projects[@]}"
 do
 	AX_SCENARIOS_DIR="$ArmarX_DIR/$i/scenarios"
 	AX_SCENARIO_PATH="$AX_SCENARIOS_DIR/$1"
-	for DIR in $AX_SCENARIOS_DIR/*/ 
+	for DIR in $AX_SCENARIOS_DIR/*/ $AX_SCENARIOS_DIR/test/*/ $AX_SCENARIOS_DIR/tests/*/ 
 	do
-		DIR_LC=${DIR,,}
-		if [ "${DIR_LC:0:${#AX_SCENARIO_PATH}}" = ${AX_SCENARIO_PATH,,} ] 
+		DIR_BASE_LC=`basename ${DIR,,}`
+		if [[ "$DIR_BASE_LC" = ${1,,}* ]] 
 		then
 			cd "$DIR"
-			AX_SCENARIO_FOUND=true;
+			AX_SCENARIO_FOUND=1;
 			break
 		fi
 	done
-	if [ "$AX_SCENARIO_FOUND" = true ] 
+	if [ $AX_SCENARIO_FOUND = 1 ] 
 	then 
 		break 
 	fi
 done
 
+if [ $AX_SCENARIO_FOUND = 0 ]
+then
+	for DIR in $AX_SCENARIOS_DIR/*/ $AX_SCENARIOS_DIR/test/*/ $AX_SCENARIOS_DIR/tests/*/ 
+	do
+		DIR_BASE_LC=`basename ${DIR,,}`
+		if [[ "$DIR_BASE_LC" = *${1,,}* ]] 
+		then
+			cd "$DIR"
+			break
+		fi
+	done
+fi
+
 unset IFS
 unset projects
 unset DIR
-unset DIR_LC
+unset DIR_BASE_LC
 unset AX_SCENARIO_FOUND
 unset AX_SCENARIOS_DIR
 unset AX_SCENARIO_PATH
