86460e23297dc5ccd8daf4063693d0f75cba5c0a
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2018-12-10 12:50:38 +0100

    updated Packages

--- _tools/clone.sh
+++ _tools/clone.sh
@@ -4,7 +4,7 @@ git clone https://gitlab.com/ArmarX/ArmarXDoc.git
 git clone https://gitlab.com/ArmarX/ArmarXCore.git
 git clone https://gitlab.com/ArmarX/ArmarXGui.git
 git clone https://gitlab.com/ArmarX/RobotAPI.git
-git clone https://gitlab.com/ArmarX/TrainX.git
+#git clone https://gitlab.com/ArmarX/TrainX.git
 git clone https://gitlab.com/ArmarX/MemoryX.git
 git clone https://gitlab.com/ArmarX/ArmarXDB.git
 git clone https://gitlab.com/ArmarX/VisionX.git
@@ -14,7 +14,7 @@ git clone https://gitlab.com/ArmarX/RobotSkillTemplates.git
 
 git clone https://i61wiki.itec.uka.de/git/spoac Spoac
 git clone https://i61wiki.itec.uka.de/git/speechx SpeechX
-git clone https://i61wiki.itec.uka.de/git/armar3 Armar3
+#git clone https://i61wiki.itec.uka.de/git/armar3 Armar3
 #git clone https://i61wiki.itec.uka.de/git/armar4 Armar4
 #git clone https://i61wiki.itec.uka.de/git/armar4-rt Armar4-RT
 git clone https://i61wiki.itec.uka.de/git/armar6rt Armar6RT
